538ee5b7c0fcadeb8174b5a5b5418fc18aa5776c
Ege Altay <35345255+byaltays@users.noreply.github.com>
2025-10-03 20:43:31 +0300

    işleme.2

---
+++ content/academito.md
@@ -0,0 +1,19 @@
+Title: Academito -Study Timer
+Date: 2025-10-03
+Category: Apps
+
+# App links:
+
+[For Android](https://play.google.com/store/apps/details?id=com.egealtay.academito)
+
+[For IOS]()
+
+
+
+# For other apps:
+
+[ANDROID](https://play.google.com/store/apps/developer?id=Ege+Altay)
+
+[IOS]()
+
+For support mail to ege.altay.dev@gmail.com
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Ege Altay'
+SITENAME = 'Ege Altay'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Istanbul'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+    ("Instagram dr.egealtay", "https://www.instagram.com/dr.egealtay/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
